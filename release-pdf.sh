32645adc93c12e48c5d5efbb5144d69ed1860537
Alex Savage <alexander.savage@iflix.com>
2018-02-06 10:57:54 +1100

    added a comment into the script. there is a docker/buildkite specific line. its not ideal

--- release-pdf.sh
+++ release-pdf.sh
@@ -19,6 +19,7 @@ fi
 
 FILENAME=$FILENAME-$2--Release-Notes
 echo $FILENAME
+##this line is needed for the docer and artifact integration comment out if running locally
 cd workspace
 gem install bundler
 bundle install && bundle exec ruby release-notes.rb $2 $REPO> $FILENAME.md
